2a3f8750abe03f0bb0e9047541ef3fcf7e805372
NozukiJ <junichi.nozuki@pathoslogos.co.jp>
2024-06-26 18:27:28 +0900

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app/app-routing.module';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { CommonModule } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app-routing.module';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
-      BrowserModule,
-      BrowserAnimationsModule,
-      FormsModule,
-      MatDialogModule,
-      HttpClientModule,
-      AppRoutingModule,
-      CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
-      CommonModule
-    )
+    provideHttpClient(),
+    provideRouter(routes)
   ]
 }).catch(err => console.error(err));
